131e356f4136f0a57d562ad97ba2c789915d128d
Necro-Coder <151525856+Necro-Coder@users.noreply.github.com>
2024-09-03 14:13:53 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Necro-Coder/portfolio/',
+  base: '/portfolio/', // Reemplaza esto por la ruta en la que GitHub Pages servirá tu sitio
+  build: {
+    outDir: 'dist',
+  },
 })
